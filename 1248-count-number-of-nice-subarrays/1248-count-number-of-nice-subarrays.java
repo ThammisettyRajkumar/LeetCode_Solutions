cbe03ca63d778ca7bd532a041af1dc977d83e25a
Raj Kumar <105033589+ThammisettyRajkumar@users.noreply.github.com>
2025-03-15 20:18:42 +0530

    Time: 41 ms (18.06%), Space: 55.3 MB (34.21%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -4,6 +4,7 @@ class Solution {
         HashMap<Integer,Integer> map = new HashMap<>();
        for(int i=0; i<nums.length; i++){
         if(nums[i] % 2 ==0) nums[i] =0;
+        else nums[i] =1;
        }
 
        int sum =0;

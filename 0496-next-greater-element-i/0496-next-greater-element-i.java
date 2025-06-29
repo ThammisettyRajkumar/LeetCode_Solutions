6b8b73c409802dd4497036f36d4994161052d522
Raj Kumar <105033589+ThammisettyRajkumar@users.noreply.github.com>
2025-06-29 19:28:47 +0530

    Time: 21 ms (5.69%), Space: 44.6 MB (18.06%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,30 +1,30 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> stack = new Stack<>();
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0; i<nums2.length; i++){
-           if(!stack.isEmpty()){
-            while(!stack.isEmpty() &&stack.peek() < nums2[i]){
-                int x = stack.pop();
-                
-                map.put(x, nums2[i]);
-            }
-           
+        
+        int[] ans = new int[nums2.length];
 
-           }
-           stack.push(nums2[i]);
+        Stack<Integer> mono = new Stack<>();
+        HashMap<Integer,Integer> index = new HashMap<>();
+        for(int i=nums2.length-1; i>=0; i--){
+            int n = nums2[i];
+            index.put(n,i);
+                while(!mono.isEmpty() &&n > mono.peek()){
+                    mono.pop();
+                }
+                if(!mono.isEmpty()){
+                    ans[i] = mono.peek();
+                }
+                else{
+                    ans[i] = -1;
+                }
+                mono.push(n);
+            
         }
-
-       
+        for(int x : ans) System.out.print(x + " ");
 
         for(int i=0; i<nums1.length; i++){
-            if(map.containsKey(nums1[i]) )
-            nums1[i] = map.get(nums1[i]);
-            else{
-                nums1[i] = -1;
-            }
+            nums1[i] = ans[index.get(nums1[i])];
         }
         return nums1;
-
     }
 }
